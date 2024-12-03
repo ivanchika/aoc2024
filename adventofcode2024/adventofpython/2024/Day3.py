956f62d32cbe060af1fbfad5ff737c1841ec8fa3
ivanchika <ivanchika@gmail.com>
2024-12-03 07:08:49 +0100

    Day 3

---
+++ adventofcode2024/adventofpython/2024/Day3.py
@@ -0,0 +1,34 @@
+from Day0 import Day0
+import re
+
+
+def calculate(line):
+    res = 0
+    for gem in re.findall(r'mul\([0-9]{1,3},[0-9]{1,3}\)', line):
+        numbers = re.findall(r'[0-9]{1,3}', gem)
+        res += int(numbers[0]) * int(numbers[1])
+    return res
+
+
+class Day3(Day0):
+
+    def part_one(self):
+        print(calculate(self.read_line()))
+
+    def part_two(self):
+        res = 0
+        for item in re.split(r"do\(\)", self.read_line()):
+            res += calculate(re.split(r"don't\(\)", item)[0])
+        print(res)
+
+
+day='3'
+
+print('Day3 Part1 Test')
+Day3('2024', 'day' + day + '_test.txt').part_one()
+print('Day3 Part1')
+Day3('2024', 'day' + day + '.txt').part_one()
+print('Day3 Part2 Test')
+Day3('2024', 'day' + day + '_test2.txt').part_two()
+print('Day3 Part2')
+Day3('2024', 'day' + day + '.txt').part_two()
