f11e013ec6b74aed843bc497d0df6a1c47b96d7c
ivanchika <ivanchika@gmail.com>
2024-12-07 08:03:25 +0100

    Day 7

--- adventofcode2024/adventofpython/2024/Day7.py
+++ adventofcode2024/adventofpython/2024/Day7.py
@@ -1,15 +1,52 @@
 from Day0 import Day0
-
+from itertools import product
+import operator
 
 class Day7(Day0):
 
     def part_one(self):
+        operators = {
+            '+': operator.add,
+            '*': operator.mul
+        }
+
         res = 0
+        for line in self.input:
+            value = int(line.split(': ')[0])
+            test = line.split(': ')[1].split(' ')
+            pm = [list(l) for l in product(['+', '*'], repeat=len(test) - 1)]
+            for op in pm:
+                calc = int(test[0])
+                for i in range(1, len(test)):
+                    calc = operators.get(op[i-1])(calc, int(test[i]))
+                if calc == value:
+                    res += value
+                    break
         print(res)
 
 
+
     def part_two(self):
+        operators = {
+            '+': operator.add,
+            '*': operator.mul,
+            '||': lambda a, b : operator.concat(str(a), str(b))
+        }
+
         res = 0
+        for line in self.input:
+            split = line.split(': ')
+            value = int(split[0])
+            test = split[1].split(' ')
+            pm = [list(l) for l in product(['+', '*', '||'], repeat=len(test) - 1)]
+
+            for op in pm:
+                calc = int(test[0])
+                for i in range(1, len(test)):
+                    calc = int(operators.get(op[i-1])(calc, int(test[i])))
+                if calc == value:
+                    res += value
+                    break
         print(res)
 
 
