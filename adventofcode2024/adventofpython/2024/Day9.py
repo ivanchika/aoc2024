2f0fa0625ee9c76a92d9e3f48aa7989b574a45b7
ivanchika <ivanchika@gmail.com>
2024-12-11 12:53:30 +0100

    Day 9 Part 2

--- adventofcode2024/adventofpython/2024/Day9.py
+++ adventofcode2024/adventofpython/2024/Day9.py
@@ -1,18 +1,5 @@
 from Day0 import Day0
 
-def print_line(pairs):
-    print('line=', build_line(pairs))
-
-def build_line(pairs):
-    return ''.join('.' if pair[0] == -1 else str(pair[0]) for pair in pairs for _ in range(int(pair[1])))
-
-def calculate(pairs):
-    res = 0
-    line = ''.join('.' if pair[0] == -1 else str(pair[0]) for pair in pairs for _ in range(int(pair[1])))
-    for i in range(len(line)):
-        res += 0 if line[i] == '.' else i * int(line[i])
-    return res
-
 class Day9(Day0):
 
     def part_one(self):
@@ -34,7 +21,32 @@ class Day9(Day0):
 
 
     def part_two(self):
-        pass
+        items = list(self.input[0])
+        cells = []
+        idx = 0
+        for i in range(len(items)):
+            if items[i] == '0':
+                continue
+            cells.append([idx if i % 2 == 0 else '.' for _ in range(int(items[i]))])
+            if i % 2 == 0:
+                idx +=1
+
+        for cell_idx in reversed(range(len(cells))):
+            cell = cells[cell_idx]
+            if '.' in cell:
+                continue
+            spaces = [c for c in cells if c.count('.') >= len(cell)]
+            if spaces:
+                space_idx = cells.index(spaces[0])
+                if cell_idx < space_idx:
+                    continue
+                space_from = spaces[0].index('.')
+                for i in range(len(cell)):
+                    spaces[0][i + space_from] = cell[i]
+                    cell[i] = '.'
+
+        res = sum(int(el) * i if el.isdigit() else 0 for i, el in enumerate(list(str(item) for c in cells for item in c)))
+        print(res)
 
 day='9'
 
@@ -42,7 +54,7 @@ print('Day9 Part1 Test')
 Day9('2024', 'day' + day + '_test.txt').part_one()
 print('Day9 Part1')
 Day9('2024', 'day' + day + '.txt').part_one()
-# print('Day9 Part2 Test')
-# Day9('2024', 'day' + day + '_test.txt').part_two()
-# print('Day9 Part2')
-# Day9('2024', 'day' + day + '.txt').part_two()
+print('Day9 Part2 Test')
+Day9('2024', 'day' + day + '_test.txt').part_two()
+print('Day9 Part2')
+Day9('2024', 'day' + day + '.txt').part_two()
